577950af02635b571653745962a93b5824353eee
camille <camille@localhost>
2019-12-24 11:16:20 +0100

    Creation of Tile and Territory classes
    Tile v1.0
    Territory v1.0

--- src/dicewars/game/Territory.java
+++ src/dicewars/game/Territory.java
@@ -8,5 +8,43 @@ package dicewars.game;
  *
  */
 public class Territory {
+	
+	//instances
+	int dicesCount;
+	int owner;
+	Territory neighbours[];
+	
+	//constructor
+	public Territory(int owner, int dicesCount, Territory neighbours[]) {
+		this.owner = owner;
+		this.dicesCount = dicesCount;
+		this.neighbours = neighbours;
+		
+		
+	
+}
 
+	public int getDicesCount() {
+		return dicesCount;
+	}
+
+	public void setDicesCount(int dicesCount) {
+		this.dicesCount = dicesCount;
+	}
+
+	public Territory[] getNeighbours() {
+		return neighbours;
+	}
+
+	public void setNeighbours(Territory[] neighbours) {
+		this.neighbours = neighbours;
+	}
+
+	public void addNeighbours(Territory newNeighbour) {
+		this.neighbours[this.neighbours.length] = newNeighbour;
+		
+	}
+	public void addDice() {
+		this.dicesCount ++;
+	}
 }
--- src/dicewars/game/Tile.java
+++ src/dicewars/game/Tile.java
@@ -8,5 +8,54 @@ package dicewars.game;
  *
  */
 public class Tile {
+	
+	//Instances
+	Territory territory;
+	Tile adjacent;
+	
+	//Constructor
+	public Tile(Territory territory, Tile adjacent) {
+		this.adjacent = adjacent;
+		this.territory = territory;
+	}
+
+	public enum Side{
+		UPLEFT,
+		UPRIGHT,
+		RIGHT,
+		DOWNRIGHT,
+		DOWNLEFT,
+		LEFT;
+	}
+	public Territory getTerritory() {
+		return territory;
+	}
+	public void setTerritory(Territory territory) {
+		this.territory = territory;
+	}
+	public Tile getAdjacent(Side side) {
+		return adjacent;
+	}
+	public void setAdjacent(Tile adjacent , Side side) {
+		this.adjacent = adjacent;
+	}
+	//Testing of the different functions
+	/*public static void main(String[] args) {
+		Territory t1 = new Territory(1,3,null);
+		Territory t2 = new Territory(2,4,null);
+		t1.addNeighbours(t2);
+		Tile Tile1 = new Tile(t1,null);
+		Tile Tile2 = new Tile(t2,Tile1);
+		Tile1.adjacent = Tile2;
+		
+		System.out.println("Owner: "+ t1.owner + "\nDices Count: "+ t1.dicesCount);
+		
+		System.out.println("Owner: "+ Tile1.territory.owner + "\nDices Count: "+ Tile1.territory.dicesCount);
+		
+		Tile1.setTerritory(t2);
+		
+		System.out.println("Owner: "+ Tile1.adjacent.territory.owner + "\nDices Count: "+ Tile1.adjacent.territory.dicesCount);
+	}*/
+	
 
 }
