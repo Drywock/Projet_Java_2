3d8d7c787a66ecf54afb68b2e5b609fc5b035a02
Thomas LINTANF <thomas.lintanf@efrei.fr>
2020-01-25 22:29:03 +0100

    Améloration des menus

--- src/dicewars/ui/MainWindow.java
+++ src/dicewars/ui/MainWindow.java
@@ -10,6 +10,7 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -21,168 +22,131 @@ import javax.swing.JTextField;
 public class MainWindow extends JFrame{
 
 
-	private static void MainWindow(){
-		
+	public MainWindow(){
 		//Création de la frame
-	    JFrame frame = new JFrame("DiceWars");
-	    frame.setSize(1920, 1200);
-	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    frame.setLocationRelativeTo(null);
-	    
-	    
-	    //Création des boutons
-	    JButton quit = new JButton("Quitter");
-	    quit.setPreferredSize(new Dimension(100,70));
-	    quit.setMinimumSize(new Dimension(100,70));
-	    quit.setMaximumSize(new Dimension(100,70));
-	    JButton multi = new JButton("Multijoueur");
-	    multi.setPreferredSize(new Dimension(250,100));
-	    multi.setMinimumSize(new Dimension(250,100));
-	    multi.setMaximumSize(new Dimension(250,100));
-	    JButton solo = new JButton("Hors ligne");
-	    solo.setPreferredSize(new Dimension(250,100));
-	    solo.setMinimumSize(new Dimension(250,100));
-	    solo.setMaximumSize(new Dimension(250,100));
-	    JButton back = new JButton("Retour");
-	    back.setPreferredSize(new Dimension(100,70));
-	    back.setMinimumSize(new Dimension(100,70));
-	    back.setMaximumSize(new Dimension(100,70));
-	    JButton back2 = new JButton("Retour");
-	    back2.setPreferredSize(new Dimension(100,70));
-	    back2.setMinimumSize(new Dimension(100,70));
-	    back2.setMaximumSize(new Dimension(100,70));
-	    JButton play = new JButton("Jouer");
-	    play.setPreferredSize(new Dimension(100,70));
-	    play.setMinimumSize(new Dimension(100,70));
-	    play.setMaximumSize(new Dimension(100,70));
-	    JButton play2 = new JButton("Jouer");
-	    play2.setPreferredSize(new Dimension(100,70));
-	    play2.setMinimumSize(new Dimension(100,70));
-	    play2.setMaximumSize(new Dimension(100,70));
-	    
-	    
-	    //Crée un liste déroulante
-	    JComboBox liste1;
-	    Object[] nbOrdi = new Object[]{"1 ordi", "2 ordi", "3 ordi", "4 ordi", "5 ordi ", "6 ordi", "7 ordi"};
-	    liste1 = new JComboBox(nbOrdi);
-	    JComboBox liste2;
-	    Object[] nbJoueurs = new Object[] { "2 joueurs", "3 joueurs", "4 joueurs", "5 joueurs ", "6 joueurs", "7 joueurs"};
-	    liste2 = new JComboBox(nbJoueurs);
-	    
-	    
+		super("DiceWars");
+		this.setSize(1920, 1200);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+
+
+		//Création des boutons
+		JButton quit = new MenuButton("Quitter");
+		JButton multi = new MenuButton("Multijoueur");
+		JButton solo = new MenuButton("Solo");
+		JButton back = new MenuButton("Retour");
+		JButton back2 = new MenuButton("Retour");
+		JButton play = new MenuButton("Jouer");
+		JButton play2 = new MenuButton("Jouer");
+
+
+		//Crée un liste déroulante
+		final int NB_MAX_PLAYER = 8;
+		
+		String[] optOrdi = new String[NB_MAX_PLAYER - 1];
+		for (int i = 1;i < NB_MAX_PLAYER; i++)
+			optOrdi[i-1] = String.format("%d ordi",i);
+		JComboBox<String> liste1 = new JComboBox<String>(optOrdi);
+		
+		String[] optJoueurs = new String[NB_MAX_PLAYER-1];
+		for (int i = 2;i <= NB_MAX_PLAYER; i++)
+			optJoueurs[i-2] = String.format("%d joueurs",i);
+		JComboBox<String> liste2 = new JComboBox<String>(optJoueurs);
+
+
 		//Création les panneaux
-	    JPanel panel = new JPanel();
-	    JPanel panel2 = new JPanel();
-	    JPanel panel3 = new JPanel();
-	    JPanel panel4 = new JPanel();
-	    JPanel panel5 = new JPanel();
-	    JPanel panelSolo = new JPanel();
-	    JPanel panelMulti = new JPanel();
-	    
-	    
-	    //On définit le layout en lui indiquant qu'il travaillera en ligne
-	    panel2.setLayout(new BoxLayout(panel2, BoxLayout.LINE_AXIS));
-	    panel2.add(solo);
-	    panel2.add(Box.createRigidArea(new Dimension(100,0)));
-	    panel2.add(multi);
-	    
-	    //On définit le layout en lui indiquant qu'il travaillera en ligne
-	    panel3.setLayout(new BoxLayout(panel3, BoxLayout.LINE_AXIS));
-	    panel3.add(quit);
-	    
-	    //On définit le layout en lui indiquant qu'il travaillera en ligne
-	    panel4.setLayout(new BoxLayout(panel4, BoxLayout.LINE_AXIS));
-	    panel4.add(back);
-	    
-	    //On définit le layout en lui indiquant qu'il travaillera en ligne
-	    panel5.setLayout(new BoxLayout(panel5, BoxLayout.LINE_AXIS));
-	    panel5.add(back2);
-	    
-	    
-	    //On positionne maintenant les deux lignes en colonne
-	    panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-	    panel.add(Box.createRigidArea(new Dimension(0,300)));
-	    panel.add(panel2);
-	    panel.add(Box.createRigidArea(new Dimension(0,100)));
-	    panel.add(panel3);
-	    
-	    //On positionne maintenant les boutons et la zone de texte avec un FlowLayout
-	    panelSolo.setLayout(new FlowLayout());
-	    panelSolo.add(Box.createRigidArea(new Dimension(0,900)));
-	    panelSolo.add(panel4);
-	    
-	    //Crée une zone de texte et pas un bouton
-	    panelSolo.add(new JTextField("Nombre de d'ordi :"));
-	    
-	    panelSolo.add(liste1);
-	    panelSolo.add(play);
+		JPanel panel = new MenuPanel("./background.png");
+		JPanel panel4 = new JPanel();
+		JPanel panel5 = new JPanel();
+		JPanel panelSolo = new MenuPanel("./background.png");
+		JPanel panelMulti = new MenuPanel("./background.png");
 
-	    //On positionne maintenant les boutons et la zone de texte avec un FlowLayout
-	    panelMulti.setLayout(new FlowLayout());
-	    panelMulti.add(Box.createRigidArea(new Dimension(0,900)));
-	    panelMulti.add(panel5);
-	    
-	    //Crée une zone de texte et pas un bouton
-	    panelMulti.add(new JTextField("Nombre de joueurs :"));
-	    
-	    panelMulti.add(liste2);
-	    panelMulti.add(play2);
-	    
-	 
-	    
-	    frame.setContentPane(panel);
-	    frame.setVisible(true);
+		//On définit le layout en lui indiquant qu'il travaillera en ligne
+		panel4.setLayout(new BoxLayout(panel4, BoxLayout.LINE_AXIS));
+		panel4.add(back);
+
+		//On définit le layout en lui indiquant qu'il travaillera en ligne
+		panel5.setLayout(new BoxLayout(panel5, BoxLayout.LINE_AXIS));
+		panel5.add(back2);
 
 
-	    solo.addActionListener(new ActionListener() {
-	         public void actionPerformed(ActionEvent e) {
-	        	frame.repaint();
-	        	frame.setContentPane(panelSolo);
-	        	frame.setVisible(true);
-	      }
-	      });
-	    
-	    back.addActionListener(new ActionListener() {
-	         public void actionPerformed(ActionEvent e) {
-	        	frame.repaint();
-		        frame.setContentPane(panel);
-		        frame.setVisible(true);
-	  }
-	    });
-	    
-	    multi.addActionListener(new ActionListener() {
-	         public void actionPerformed(ActionEvent e) {
-	        	frame.repaint();
-		        frame.setContentPane(panelMulti);
-		        frame.setVisible(true);
-	  }
-	    });
-	    back2.addActionListener(new ActionListener() {
-	         public void actionPerformed(ActionEvent e) {
-	        	frame.repaint();
-		        frame.setContentPane(panel);
-		        frame.setVisible(true);
-	         } });
-	    
+		//On positionne maintenant les deux lignes en colonne
+		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+		panel.add(Box.createRigidArea(new Dimension(0,300)));
+		panel.add(solo);
+		panel.add(Box.createRigidArea(new Dimension(0,50)));
+		panel.add(multi);
+		panel.add(Box.createRigidArea(new Dimension(0,50)));
+		panel.add(quit);
 
-	    quit.addActionListener(new ActionListener() {
-	         public void actionPerformed(ActionEvent e) {
-	        	frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
-		        frame.setContentPane(panelMulti);
-		        frame.setVisible(true);
-	  }
-	    });
+		//On positionne maintenant les boutons et la zone de texte avec un FlowLayout
+		panelSolo.setLayout(new FlowLayout());
+		panelSolo.add(Box.createRigidArea(new Dimension(0,900)));
+		panelSolo.add(panel4);
+
+		//Crée une zone de texte et pas un bouton
+		panelSolo.add(new JLabel("Nombre de d'ordi :"));
+
+		panelSolo.add(liste1);
+		panelSolo.add(play);
+
+		//On positionne maintenant les boutons et la zone de texte avec un FlowLayout
+		panelMulti.setLayout(new FlowLayout());
+		panelMulti.add(Box.createRigidArea(new Dimension(0,900)));
+		panelMulti.add(panel5);
+
+		//Crée une zone de texte et pas un bouton
+		panelMulti.add(new JTextField("Nombre de joueurs :"));
+
+		panelMulti.add(liste2);
+		panelMulti.add(play2);
+
+		ActionListener goBackListener = new SwitchPanelActionListener(panel);
+		ActionListener goToSoloListener = new SwitchPanelActionListener(panelSolo);
+		ActionListener goToMultiListener = new SwitchPanelActionListener(panelMulti);
+
+		solo.addActionListener(goToSoloListener);
+		back.addActionListener(goBackListener);
+		multi.addActionListener(goToMultiListener);
+		back2.addActionListener(goBackListener);
+
+		quit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MainWindow.this.dispatchEvent(new WindowEvent(MainWindow.this, WindowEvent.WINDOW_CLOSING));
+				MainWindow.this.setContentPane(panelMulti);
+				MainWindow.this.setVisible(true);
+			}
+		});
+		
+		this.setContentPane(panel);
+		this.setVisible(true);
 
 	}
 	
+	public class SwitchPanelActionListener implements ActionListener{
+		private JPanel panel;
+		
+		public SwitchPanelActionListener(JPanel panel) {
+			this.panel = panel;
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			MainWindow.this.setContentPane(panel);
+			MainWindow.this.repaint();
+			MainWindow.this.revalidate();;
+		}
+		
+	}
+
 
 	/**
-     * Construct the method main
-     * @version 1.0
-     */
+	 * Construct the method main
+	 * @version 1.0
+	 */
 	public static void main(String[] args) {
-		MainWindow();
+		new MainWindow();
 
 	}
-	
+
 }
